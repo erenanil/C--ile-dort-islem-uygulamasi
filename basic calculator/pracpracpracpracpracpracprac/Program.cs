5d99592b6da45c645021fa482572c67a5b19e584
Anıl EREN <anileren_w@hotmail.com>
2023-12-01 08:30:08 +0300

    basic calculator for beginners
    A basic beginner-level calculator that performs the four basic arithmetic operations, doesn't encounter division by zero, and handles decimal parts seamlessly.

---
+++ basic calculator/pracpracpracpracpracpracprac/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics;
+using System.Security.Cryptography;
+
+namespace pracpracpracpracpracpracprac
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            calculate clc = new calculate();
+            Console.WriteLine("Girdiğiniz iki sayıyla dört işlem yapan programa Hoş geldiniz!");
+            Console.WriteLine("Lütfen ilk değeri giriniz: ");
+            int s1 = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Lütfen ikinci değeri giriniz: ");
+            int s2 = Convert.ToInt16(Console.ReadLine());
+
+            clc.carp(s1, s2);
+            if (s2 != 0)
+            {
+                clc.bol(s1, s2);
+            }
+            else
+            {
+                Console.WriteLine("Bölümü: Sıfıra bölme işlemi tanımsızdır.");
+            }
+            clc.fark(s1, s2);
+            clc.topla(s1, s2);
+            Console.Read();
+        }
+    }
+}
---
+++ basic calculator/pracpracpracpracpracpracprac/calculate.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pracpracpracpracpracpracprac
+{
+    internal class calculate
+    {
+        public int topla(int a,int b)
+        {
+            int c=a+b;
+            Console.WriteLine("Toplamı: "+c);
+            return c;
+        }
+        public int fark (int a,int b)
+        {
+            int c=a-b;
+            Console.WriteLine("Farkı: "+c);
+            return c;
+        }
+        public int carp(int a,int b)
+        {
+            int c=a*b;
+            Console.WriteLine("Çarpımı: "+c);
+            return c;
+        }
+        //iki tam sayının bölümü yine bir tam sayı olarak değerlendirilir.
+        //Dolayısıyla, double türündeki bir değişkene atama yapılsa bile bu, bölme işleminin gerçek ondalık sonucunu vermeyecektir.
+        //Bu yüzden en az bir değişkeni double türünde yazıyoruz.
+        public double bol(double a,int b)
+        {
+            double c = a / b;
+            Console.WriteLine("Bölümü: " +c.ToString("0.00"));
+            return c;
+        }
+
+
+
+
+    }
+}
